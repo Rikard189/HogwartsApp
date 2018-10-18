d6d6c1d7202d93fc307cb11b7fff19d755907f88
Ricardo Acosta <rikard189@gmail.com>
2018-10-18 14:13:26 -0500

    Modifies student views

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ApplicationRecord
   belongs_to :course
   belongs_to :professor
   validate :not_repollo
-
+  has_and_belongs_to_many :students
   def not_repollo
     if description.include?("repollo")
       errors.add(:description, "No puedes poner repollo")
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :department
   validate :not_hufflepuff
-
+  has_and_belongs_to_many :sections
   def not_hufflepuff
     if (house == "Hufflepuff")
        errors.add(:house, "We dont accept Hufflepuff")
